f10dae6d72f2c1d4a915ab391a6d1cf8aee3af87
Borislav Stanimirov <b.stanimirov@abv.bg>
2024-10-25 13:54:19 +0300

    feat: chat ops

--- app/src/main/java/com/alpacacore/example/chat/ChatActivity.kt
+++ app/src/main/java/com/alpacacore/example/chat/ChatActivity.kt
@@ -61,6 +61,9 @@ class ChatActivity : Activity() {
             val model = AlpacaCore.createModel(desc, null, null)
             instance = model.createInstance("general", null)
             AlpacaCore.releaseModel(model)
+
+            val setup = "A chat between a human user and a helpful AI assistant";
+            instance.runOp("begin-chat", mapOf("setup" to setup),null)
         }
     }
 
@@ -75,15 +78,9 @@ class ChatActivity : Activity() {
         pushMessage(ChatMsg(ChatMsg.Source.USER, userText))
 
         executor.execute {
-            val result = instance.runOp(
-                "run",
-                mapOf(
-                    "prompt" to userText,
-                    "max_tokens" to 20
-                ),
-                null
-            ) as Map<*, *>
-            val aiText = result["result"] as String
+            instance.runOp("add-chat-prompt", mapOf("prompt" to userText),null)
+            val response = instance.runOp("get-chat-response", null, null) as Map<*, *>
+            val aiText = response["response"] as String
             mainHandler.post {
                 pushMessage(ChatMsg(ChatMsg.Source.AI, aiText))
             }
